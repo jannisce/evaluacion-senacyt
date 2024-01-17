4be5d1125a062de52cc7d3d674b96c229ca3c7bb
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2024-01-16 19:30:22 -0600

    refactor: change user details component

--- src/components/UserDetailedCard/UserDetailedCard.jsx
+++ src/components/UserDetailedCard/UserDetailedCard.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
 
-const UserDetailedCard = () => {
+const UserDetailedCard = ({ user }) => {
   return (
-    <div>UserDetailedCard</div>
-  )
-}
+    <div className='bg-white shadow-md rounded-md p-6 w-full max-w-6xl mx-auto flex flex-col'>
+      <h2 className='text-4xl font-semibold mb-6'>{user.nombre_completo}</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-8 flex-grow'>
+        <div>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Nombre:</span> {user.nombre}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Apellido 1:</span> {user.apellido1}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Apellido 2:</span> {user.apellido2}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Fecha de Nacimiento:</span>{' '}
+            {user.fecha_nacimiento}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Email:</span> {user.email}
+          </p>
+        </div>
 
-export default UserDetailedCard
+        <div>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Teléfono:</span> {user.telefono}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Dirección:</span> {user.direccion}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Código Postal:</span>{' '}
+            {user.codigo_postal}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Municipio:</span> {user.municipio}
+          </p>
+          <p className='text-lg mb-4'>
+            <span className='font-semibold'>Provincia:</span> {user.provincia}
+          </p>
+        </div>
+      </div>
+      <div className='mt-8'>
+        <p className='text-lg mb-4'>
+          <span className='font-semibold'>ID:</span> {user.id}
+        </p>
+        <p className='text-lg mb-4'>
+          <span className='font-semibold'>SSN:</span> {user.ssn}
+        </p>
+        <p className='text-lg mb-4'>
+          <span className='font-semibold'>IBAN:</span> {user.iban}
+        </p>
+        <p className='text-lg mb-4'>
+          <span className='font-semibold'>Tarjeta:</span> {user.tarjeta}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserDetailedCard;
--- src/pages/Users/UserDetails.jsx
+++ src/pages/Users/UserDetails.jsx
@@ -4,6 +4,7 @@ import Layout from '../../components/Layout/Layout'
 import { useParams } from 'react-router-dom'
 import BackButton from '../../components/BackButton/BackButton'
 import Loading from '../../components/Loading/Loading'
+import UserDetailedCard from '../../components/UserDetailedCard/UserDetailedCard'
 
 const UserDetails = ( ) => {
   const { id } = useParams()
@@ -40,70 +41,8 @@ const UserDetails = ( ) => {
       {error && <p className='m-6 text-2xl'>Error al cargar los detalles del usuario: {error}</p>}
 
       {user && (
-        <div className='m-4 bg-gray-100  p-6 rounded-md shadow-md mx-auto max-w-md'>
-          <h2 className='text-2xl font-semibold mb-4'>
-            {user.nombre_completo}
-          </h2>
-
-          <div className='w-80 grid grid-cols-1 gap-4 sm:grid-cols-2'>
-            <div>
-              <p>
-                <span className='font-semibold'>Nombre:</span> {user.nombre}
-              </p>
-              <p>
-                <span className='font-semibold'>Apellido 1:</span>{' '}
-                {user.apellido1}
-              </p>
-              <p>
-                <span className='font-semibold'>Apellido 2:</span>{' '}
-                {user.apellido2}
-              </p>
-              <p>
-                <span className='font-semibold'>Fecha de Nacimiento:</span>{' '}
-                {user.fecha_nacimiento}
-              </p>
-              <p>
-                <span className='font-semibold'>Email:</span> {user.email}
-              </p>
-            </div>
-
-            <div>
-              <p>
-                <span className='font-semibold'>Teléfono:</span> {user.telefono}
-              </p>
-              <p>
-                <span className='font-semibold'>Dirección:</span>{' '}
-                {user.direccion}
-              </p>
-              <p>
-                <span className='font-semibold'>Código Postal:</span>{' '}
-                {user.codigo_postal}
-              </p>
-              <p>
-                <span className='font-semibold'>Municipio:</span>{' '}
-                {user.municipio}
-              </p>
-              <p>
-                <span className='font-semibold'>Provincia:</span>{' '}
-                {user.provincia}
-              </p>
-            </div>
-          </div>
-
-          <div className='mt-4'>
-            <p>
-              <span className='font-semibold'>ID:</span> {user.id}
-            </p>
-            <p>
-              <span className='font-semibold'>SSN:</span> {user.ssn}
-            </p>
-            <p>
-              <span className='font-semibold'>IBAN:</span> {user.iban}
-            </p>
-            <p>
-              <span className='font-semibold'>Tarjeta:</span> {user.tarjeta}
-            </p>
-          </div>
+        <div className='m-4 bg-gray-100  p-6 rounded-md shadow-md mx-auto max-w-6xl'>
+          <UserDetailedCard user={user} />
         </div>
       )}
     </Layout>
